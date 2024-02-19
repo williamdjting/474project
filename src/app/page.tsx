2b08f3d7ce1074cd1b67ea6255e838b328597035
William Ting <williamdjting@gmail.com>
2024-02-18 17:26:38 -0800

    added mapping of list array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,54 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      
+  const [list, setList] = useState<string>("");
 
-      <div className={styles.center}>
-        <ul>
-          <div>
-            <div className={styles.center1}>
-              Hello
-            </div>
+  const [submittedValues, setSubmittedValues] = useState<string[]>([]);
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+
+    console.log('Input Text:', list);
+    // Add the value of "e" to the list of submitted values
+    setSubmittedValues([...submittedValues, list]);
+
+  };
+
+
+  return (
+    <main className={styles.center}>
+      <div className={styles.center1}>
+
+        <form onSubmit={handleFormSubmit}>
+          {/* Input for text */}
+          <label>
+            Text:
+            <input
+              type="text"
+              value={list}
+              onChange={(e) => setList(e.target.value)}
+            />
+          </label>
+          <br />
+          {/* Input for submit */}
+          <input type="submit" value="Click me" />
+        </form>
+      </div>
+
+      {/* Display the list of submitted values */}
+
+      <div className={styles.center1}>
+        {submittedValues.map((value, index) => (
+           <div className={styles.center1}>
+          <li key={index}>{value}</li>
           </div>
-          <div>
-            <div className={styles.center1}>
-              Hello
-            </div>
-          </div>
-          <div>
-            <div className={styles.center1}>
-              Hello
-            </div>
-          </div>
-        </ul>
-    
-    </div>
+        ))}
+      </div>
 
     </main>
   );
