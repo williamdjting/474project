19fc6cad5300abf1057f766f910580e59fbdf5aa
William Ting <williamdjting@gmail.com>
2024-02-19 00:38:45 -0800

    moved to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import AppStyle from "./App.module.css";
 import React, { useState } from "react";
 
 function App() {
@@ -10,45 +9,40 @@ function App() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-
-    console.log('Input Text:', list);
+    console.log("Input Text:", list);
     // Add the value of "e" to the list of submitted values
     setSubmittedValues([...submittedValues, list]);
-
   };
 
-
   return (
-    <main className={App.center}>
-      <div className={App.center1}>
-
-        <form onSubmit={handleFormSubmit}>
-          {/* Input for text */}
-          <label>
-            Text:
-            <input
-              type="text"
-              value={list}
-              onChange={(e) => setList(e.target.value)}
-            />
-          </label>
-          <br />
-          {/* Input for submit */}
-          <input type="submit" value="Click me" />
-        </form>
+    <>
+      <div className={AppStyle.center}>
+        <div className={AppStyle.center1}>
+          <form onSubmit={handleFormSubmit}>
+            {/* Input for text */}
+            <label>
+              Text:
+              <input
+                type="text"
+                value={list}
+                onChange={(e) => setList(e.target.value)}
+              />
+            </label>
+            <br />
+            {/* Input for submit */}
+            <input type="submit" value="Click me" />
+          </form>
+        </div>
       </div>
 
-      {/* Display the list of submitted values */}
-
-      <div className={App.center1}>
+      <div className={AppStyle.center}>
         {submittedValues.map((value, index) => (
-           <div className={App.center1}>
-          <li key={index}>{value}</li>
+          <div className={AppStyle.center1}>
+            <li key={index}>{value}</li>
           </div>
         ))}
       </div>
-
-    </main>
+    </>
   );
 }
 
