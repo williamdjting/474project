be924824d7e8beb079030737fab18dcaa7b12f0e
William Ting <williamdjting@gmail.com>
2024-02-18 16:56:55 -0800

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+
+      <div className={styles.center}>
+        <ul>
+          <div>
+            <div className={styles.center1}>
+              Hello
+            </div>
+          </div>
+          <div>
+            <div className={styles.center1}>
+              Hello
+            </div>
+          </div>
+          <div>
+            <div className={styles.center1}>
+              Hello
+            </div>
+          </div>
+        </ul>
+    
+    </div>
+
+    </main>
+  );
+}
