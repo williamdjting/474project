de037e10b676861d5952cb03e72823f727122051
William Ting <williamdjting@gmail.com>
2024-02-20 16:20:31 -0800

    Update ViewItem.js

--- src/components/ViewItem.js
+++ src/components/ViewItem.js
@@ -8,17 +8,17 @@ function View() {
 
   const { listArray } = useContext(ArrayContext);
 
-  const [list, setList] = useState("");
+  // const [list, setList] = useState("");
 
-  const [submittedValues, setSubmittedValues] = useState([]);
+  // const [submittedValues, setSubmittedValues] = useState([]);
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
 
-    console.log("Input Text:", list);
-    // Add the value of "e" to the list of submitted values
-    setSubmittedValues([...submittedValues, list]);
-  };
+  //   console.log("Input Text:", list);
+  //   // Add the value of "e" to the list of submitted values
+  //   setSubmittedValues([...submittedValues, list]);
+  // };
 
   return (
     <>
@@ -34,35 +34,6 @@ function View() {
       </ul>
         </div>
       </div>
-
-      
-      {/* <div className={ViewStyle.centerViewItem}>
-        <div className={ViewStyle.center1ViewItem}>
-          <form onSubmit={handleFormSubmit}>
-            
-            <label>
-              Text:
-              <input
-                type="text"
-                value={list}
-                onChange={(e) => setList(e.target.value)}
-              />
-            </label>
-            <br />
-
-            <input type="submit" value="Click me" />
-          </form>
-        </div>
-      </div> */}
-
-      {/* <div className={ViewStyle.centerViewItem}>
-        {submittedValues.map((value, index) => (
-          <div className={ViewStyle.center1ViewItem}>
-            <li key={index}>{value}</li>
-          </div>
-        ))}
-      </div> */}
-
 
       <Link to="/">
         <button>Go to Create Page</button>
