fdcb7bc20814a23dfc8c589e58f49410739c0428
unknown <feelingzty@gmail.com>
2024-02-29 14:42:55 -0800

    adjusted axio to new api

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
 
   useEffect(() => {
     axios
-      .get('https://opentdb.com/api_category.php')
+      .get('http://localhost:5000/category')
       .then(res => {
-        setCategories(res.data.trivia_categories)
+        setCategories(res.data)
       })
   }, [])
 
@@ -31,19 +31,11 @@ function App() {
   function handleSubmit(e) {
     e.preventDefault()
     axios
-    .get('https://opentdb.com/api.php', {
-      params: {
-        amount: amountEl.current.value,
-        category: categoryEl.current.value
-      }
-    })
+    .get('http://localhost:5000/question/' + categoryEl.current.value + '/' + amountEl.current.value)
     .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer)
-        const options = [
-          ...questionItem.incorrect_answers.map(a => decodeString(a)),
-          answer
-        ]
+      setFlashcards(res.data.map((questionItem, index) => {
+        const answer = decodeString(questionItem.answer)
+        const options = [questionItem.option1, questionItem.option2, answer]
         return {
           id: `${index}-${Date.now()}`,
           question: decodeString(questionItem.question),
@@ -61,13 +53,13 @@ function App() {
           <label htmlFor="category">Category</label>
           <select id="category" ref={categoryEl}>
             {categories.map(category => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
+              return <option value={category.id} key={category.id}>{category.category_name}</option>
             })}
           </select>
         </div>
         <div className="form-group">
           <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+          <input type="number" id="amount" min="1" step="1" defaultValue={3} ref={amountEl} />
         </div>
         <div className="form-group">
           <button className="btn">Generate</button>
