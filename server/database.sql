580fbf6812244cb45c9bd8f251bc0689a7fc9917
unknown <feelingzty@gmail.com>
2024-02-28 17:05:02 -0800

    api for creating and getting categories and questions

---
+++ server/database.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE flashcard_db;
+
+CREATE TABLE users(
+    username VARCHAR(32) PRIMARY KEY,
+    password_text VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE category(
+    category_name VARCHAR(32) PRIMARY KEY
+);
+
+CREATE TABLE flashcard(
+    question_id SERIAL PRIMARY KEY,
+    question VARCHAR(255) NOT NULL UNIQUE,
+    option1 VARCHAR(32) NOT NULL,
+    option2 VARCHAR(32) NOT NULL,
+    answer VARCHAR(32) NOT NULL,
+    category_name VARCHAR(32),
+    owner_name VARCHAR(32) NOT NULL,
+    CONSTRAINT fk_category
+        FOREIGN KEY(category_name) 
+	    REFERENCES category(category_name)
+	    ON DELETE CASCADE,
+    CONSTRAINT fk_user
+        FOREIGN KEY(owner_name) 
+	    REFERENCES users(username)
+	    ON DELETE CASCADE,
+    CONSTRAINT question_unique 
+        UNIQUE (question)
+);
---
+++ server/server.js
@@ -0,0 +1,62 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+const cors = require("cors");
+const pool = require("./db")
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000; // Use the port specified in environment variables or default to 5000
+
+// Additional API routes or other server logic can be added here
+
+//Create a category
+app.post("/category", async(req, res)=>{
+  try {
+      const {category_name} = req.body;
+      const newCategory = await pool.query("INSERT INTO category (category_name) VALUES ($1) RETURNING *", [category_name]);
+      res.json(newCategory.rows[0]);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+//Get all categories
+app.get("/category", async(req, res)=>{
+  try {
+      const allCategories = await pool.query("SELECT * FROM category");
+      res.json(allCategories.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+//create a question
+app.post("/question", async(req, res)=>{
+  try {
+      const {category_name, owner_name, question, option1, option2, answer} = req.body;
+      const newQuestion = await pool.query(
+      `INSERT INTO flashcard (category_name, owner_name, question, option1, option2, answer) 
+      VALUES($1, $2, $3, $4, $5, $6) RETURNING *`, [category_name, owner_name, question, option1, option2, answer]);
+      res.json(newQuestion.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+//get all questions under a specific category
+app.get("/question/:category", async(req, res)=>{
+  try {
+      const {category} = req.params;
+      const questions = await pool.query("SELECT * FROM flashcard WHERE category_name = $1", [category]);
+      res.json(questions.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
